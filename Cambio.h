ec6116cb9d6610aef0299d1134264930a87c7bd1
A01703076 <74276761+A01703076@users.noreply.github.com>
2020-11-30 00:26:34 -0600

    Add files via upload

---
+++ Cambio.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class Cambio{
+    protected:
+        float precio;
+        float dinero;
+    public:
+        Cambio();
+        void setDinero(float);
+        float getDinero();
+        float total(float, float);
+};
+
+Cambio::Cambio(){
+}
+
+void Cambio::setDinero(float _dinero){
+    dinero = _dinero;
+}
+
+float Cambio::getDinero(){
+    return dinero;
+}
+
+float Cambio ::total(float dinero, float precio){
+    cout<<"\nSu cantidad convertida es: "<<endl; cout<<dinero/precio;
+}
