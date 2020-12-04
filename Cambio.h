db132880703f2e429c16bdb74ff102a766827c3d
A01703076 <74276761+A01703076@users.noreply.github.com>
2020-12-03 22:43:35 -0600

    Update Cambio.h

--- Cambio.h
+++ Cambio.h
@@ -1,13 +1,21 @@
+// Bibliotecas importadas
 #include <iostream>
 #include <stdlib.h>
-#ifndef CAMBIO_H_
-#define CAMBIO_H_
 using namespace std;
 
+// Include que hace que la clase no se defina más de una vez
+#ifndef CAMBIO_H_
+#define CAMBIO_H_
+
+// Crea la clase padre "Cambio"
 class Cambio{
+    
+// Atributos en protected para que se puedan modificar directamente 
     protected:
         float precio;
         float dinero;
+    
+// Metodos de la clase
     public:
         Cambio();
         void setDinero(float);
@@ -15,17 +23,21 @@ class Cambio{
         float total(float, float);
 };
 
+// Constructor de la clase cambio
 Cambio::Cambio(){
 }
 
+// Se crea la funcion del setter 
 void Cambio::setDinero(float _dinero){
     dinero = _dinero;
 }
 
+// Se crea la funcion del getter
 float Cambio::getDinero(){
     return dinero;
 }
 
+// Se crea la funcion que da el total de las operaciones
 float Cambio ::total(float dinero, float precio){
     cout<<"\nSu cantidad convertida es: "<<endl; cout<<dinero/precio;
 }
