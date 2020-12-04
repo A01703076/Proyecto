9f520c918dc2e0e01ea7dd3e44b8264bc0e3dd7c
A01703076 <74276761+A01703076@users.noreply.github.com>
2020-12-03 22:51:52 -0600

    Update MonedaL.h

--- MonedaL.h
+++ MonedaL.h
@@ -1,8 +1,13 @@
+// Importa la librerias 
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+
+// Incluye la clase "Cambio"
 #include "Cambio.h"
 
+
+// Se crea la clase monedaL que da el tipo de cambio de la libra y se pone la herencia de la clase "Cambio"
 class monedaL : public Cambio{
     private:
 
@@ -12,13 +17,16 @@ class monedaL : public Cambio{
         float getprecioL();
 };
 
+// Se crea el contructor de la clase monedaL y se pone la herencia 
 monedaL::monedaL() : Cambio(){
 }
 
+// Se crea el setter de la clase para dar el precio de la moneda de cambio de la Libra
 void monedaL::setprecioL(float _precioD){
     precio = _precioD;
 }
 
+// Se crea el getter para regresa el precio puesto en el setter
 float monedaL::getprecioL(){
     return precio;
 }
