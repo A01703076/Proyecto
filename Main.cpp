7d2f5e7416e222db154e67a4b0abd8016fde9027
A01703076 <74276761+A01703076@users.noreply.github.com>
2020-12-03 23:02:55 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -22,19 +22,22 @@ int main(){
     Cambio d1;
     cout<<"\nIngrese su cantidadd de dinero en pesos mexicanos: "<<endl; cin>>dinero;    
     d1.setDinero(dinero);
-    cout<<"\nIngrese el tipo de cambio en dolares: "<<endl; cin>>precio;
+    cout<<"\nIngrese el tipo de cambio en dolares: "<<endl; 
+    cin>>precio;
     
 // Se crea el objeto 1, que es el que da el cambio en dolares con los setters 
     monedaD m1;
     m1.setprecioD(precio);
     m1.total(dinero,precio);
-    cout<<"\nIngrese el tipo de cambio en euros: "<<endl; cin>>precio;
+    cout<<"\nIngrese el tipo de cambio en euros: "<<endl; 
+    cin>>precio;
     
 // Se crea el objeto 2, que es el que da el cambio en euros con los setters 
     monedaE m2;
     m2.setprecioE(precio);
     m2.total(dinero,precio);
-    cout<<"\nIngrese el tipo de cambio en libras: "<<endl; cin>>precio;
+    cout<<"\nIngrese el tipo de cambio en libras: "<<endl; 
+    cin>>precio;
     
 // Se crea el objeto 3, que es el que da el cambio en libras con los setters 
     monedaL m3;
