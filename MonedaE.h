8c98dee4c6ebcbe5857ac3cb0c7a91ab21b3c28e
A01703076 <74276761+A01703076@users.noreply.github.com>
2020-12-03 22:48:46 -0600

    Update MonedaE.h

--- MonedaE.h
+++ MonedaE.h
@@ -1,8 +1,12 @@
+// Se importan las bibliotecas
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+
+//Se incluye la clase padre "Cambio"
 #include "Cambio.h"
 
+// Se crea la clase monedaE poniendo la herencia de la clase "Cambio"
 class monedaE : public Cambio{
     private:
 
@@ -12,13 +16,16 @@ class monedaE : public Cambio{
         float getprecioE();
 };
 
+// Se crea el constructor de la clase monedaE junto con la herencia 
 monedaE::monedaE() : Cambio(){
 }
 
+// Se crea el setter que nos da el precio de la moneda de cambio del euro 
 void monedaE::setprecioE(float _precioD){
     precio = _precioD;
 }
 
+// Se crea el getter del precio de la moneda
 float monedaE::getprecioE(){
     return precio;
 }
